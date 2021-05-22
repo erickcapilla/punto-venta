670a1e7e99a3bbdae3698cb5a423d951b1adf7a0
ErickCapilla <erickpoblano25@gmail.com>
2021-05-22 00:35:58 -0500

    Adden script to form preference

--- script/practica_3.js
+++ script/practica_3.js
@@ -2,14 +2,25 @@ const comprar = document.querySelectorAll("#comprar_btn")
 const comprar_card = document.getElementById('compra_card')
 const registro_btn = document.getElementById('btn_registro')
 const formRegis = document.getElementById('formRegis')
-const close = document.querySelector('#close')
+const closed = document.querySelectorAll('#close')
 const btn_prefe = document.getElementById('btn_prefe')
 const btn_registrar = document.getElementById('btn_registrar')
+const formPrefe = document.getElementById('formPrefe')
 
-close.addEventListener('click', () => {
-  if(formRegis.classList.contains('is-active')) {
+btn_prefe.addEventListener('click', () => {
+  if(!formPrefe.classList.contains('is-active')) {
+    formPrefe.classList.add('is-active')
     formRegis.classList.remove('is-active')
   }
+})
+
+closed.forEach(close => {
+  close.addEventListener('click', () => {
+    if(formRegis.classList.contains('is-active') || formPrefe.classList.contains('is-active')) {
+      formRegis.classList.remove('is-active')
+      formPrefe.classList.remove('is-active')
+    }
+  })
 })
 
 registro_btn.addEventListener('click', () => {
