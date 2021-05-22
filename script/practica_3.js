25ef6c11bc7aa8bc3a6497f98402d125144171ba
ErickCapilla <erickpoblano25@gmail.com>
2021-05-21 23:31:16 -0500

    Added script to form sing up

--- script/practica_3.js
+++ script/practica_3.js
@@ -1,5 +1,20 @@
 const comprar = document.querySelectorAll("#comprar_btn")
 const comprar_card = document.getElementById('compra_card')
+const registro_btn = document.getElementById('btn_registro')
+const formRegis = document.getElementById('formRegis')
+const close = document.querySelector('#close')
+
+close.addEventListener('click', () => {
+  if(formRegis.classList.contains('is-active')) {
+    formRegis.classList.remove('is-active')
+  }
+})
+
+registro_btn.addEventListener('click', () => {
+  if(!formRegis.classList.contains('is-active')) {
+    formRegis.classList.add('is-active')
+  }
+})
 
 comprar.forEach(comprar => {
   comprar.addEventListener("click", compraConfi);
@@ -13,3 +28,4 @@ function compraConfi(e) {
     }, 3000);
   }
 }
+
